84dddf0e97575f741790b4927108b8b9ef4908ae
akhilthomasattiapillil <akhilthomasattiapillil@gmail.com>
2020-04-09 11:39:39 -0400

    Initial author routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express =require('express')
 const router =express.Router()
 
+// Initial Index Route
 router.get('/',(req,res)=> {
     res.render('index')
 })
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -8,14 +8,17 @@ const NODEENV = process.env.NODEENV;
 const express = require('express')
 const app = express()
 const expresslayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
-const indexrouter = require('./routes/index')
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine','ejs')
 app.set('views',__dirname+'/views') 
 app.set('layout','layouts/layout')
 app.use(expresslayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb' ,extended: false}))
 
 const hostname = process.env.HOST;
 const DATABASE = process.env.DATABASE;
@@ -33,6 +36,7 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 
-app.use('/',indexrouter)
+app.use('/',indexRouter)
+app.use('/authors',authorRouter)
 app.listen(process.env.PORT || 3000)
 
